5bac0d1db79a229197ddd2fd9417fc06a3e90c48
Wahome97 <d.wahome@alustudent.com>
2021-11-23 12:01:21 +0300

    Add project files.

---
+++ dowhile loop Assignment 3/dowhile loop Assignment 3.cpp
@@ -0,0 +1,22 @@
+// do..while loop Assignment 3.
+// C++ program that computes the sum of numbers between 20 and 25 using do...while loop.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Declare and initialize variable
+	int number;
+	int sum{};
+
+	// do..while loop to compute sum of numbers
+	do {
+		cout << "Please input 20 and 25 then enter 0 to stop and view sum:\n ";
+		cin >> number;
+		sum += number;
+	} while (number != 0);
+	cout << " Sum of the numbers is : " << sum << endl;
+
+	//Output when the user enters 0 to end loop
+	return 0;
+}
